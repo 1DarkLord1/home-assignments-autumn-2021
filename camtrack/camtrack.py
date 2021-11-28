d4d6445cb2160f0a3c4cacbd74f1b07743547a07
1DarkLord1 <camm2016@yandex.ru>
2021-11-28 23:55:18 +0300

    HW3: Camera positions initialization

--- camtrack/camtrack.py
+++ camtrack/camtrack.py
@@ -44,33 +44,25 @@ from _camtrack import (
     build_correspondences,
     Correspondences,
     rodrigues_and_translation_to_view_mat3x4,
-    view_mat3x4_to_pose
+    view_mat3x4_to_pose,
+    remove_correspondences_with_ids,
+    eye3x4
 )
-
-
-def build_3d_2d_correspondences(
-        points_3d: Dict,
-        corners: FrameCorners,
-) -> Correspondences:
-    assert points_3d
-
-    points_3d_ids = np.sort(np.array(list(points_3d.keys())))
-    common_ids, (_, indices_2d) = snp.intersect(
-        points_3d_ids,
-        np.sort(corners.ids.flatten()),
-        indices=True
-    )
-
-    return Correspondences(
-        common_ids,
-        np.array([points_3d[x] for x in common_ids]),
-        corners.points[indices_2d],
-    )
 
 
 FramePnPInformation = namedtuple(
     'FramePnPInformation',
     ('frame', 'inliers_ratio', 'inliers', 'corresps', 'r_vec_0', 't_vec_0')
+)
+
+EssentialMatContext = namedtuple(
+    'EssentialMatInformation',
+    ('frame_1', 'frame_2', 'E', 'correspondences')
+)
+
+ViewMatsContext = namedtuple(
+    'EssentialMatInformation',
+    ('frame_1', 'frame_2', 'view_mat_1', 'view_mat_2')
 )
 
 
@@ -86,10 +78,11 @@ class CameraTracker:
             rgb_sequence: FramesSequence,
             corner_storage: CornerStorage,
             intrinsic_mat: np.ndarray,
-            known_view_1: Optional[Tuple[int, Pose]],
-            known_view_2: Optional[Tuple[int, Pose]],
             triang_parameters: TriangulationParameters,
             other_parameters: Dict,
+            known_view_1: Optional[Tuple[int, Pose]] = None,
+            known_view_2: Optional[Tuple[int, Pose]] = None,
+            init_view_mats_parameters: Optional[Dict] = None,
     ):
         self._rgb_sequence = rgb_sequence
         self._corner_storage = corner_storage
@@ -98,6 +91,11 @@ class CameraTracker:
         self._known_view_2 = known_view_2
         self._triang_parameters = triang_parameters
         self._other_parameters = other_parameters
+        self._init_view_mats_parameters = init_view_mats_parameters
+
+        if (known_view_1 is None or known_view_2 is None) and init_view_mats_parameters is None:
+            raise ValueError('init_view_mats_parameters argument is None')
+
         self._frames_count = len(corner_storage)
         self._poses = np.empty(self._frames_count, dtype=np.ndarray)
         self._points_3d = {}
@@ -165,19 +163,48 @@ class CameraTracker:
 
         return rodrigues_and_translation_to_view_mat3x4(r_vec, t_vec)
 
+    def _build_3d_2d_correspondences(
+            self,
+            points_3d_ids: np.ndarray,
+            corners: FrameCorners,
+    ) -> Correspondences:
+        common_ids, (_, indices_2d) = snp.intersect(
+            points_3d_ids,
+            np.sort(corners.ids.flatten()),
+            indices=True
+        )
+
+        return Correspondences(
+            common_ids,
+            np.array([self._points_3d[x] for x in common_ids]),
+            corners.points[indices_2d],
+        )
+
     def _handle_best_frame_for_pnp(self) -> Tuple[BestFrameState, int]:
         """
         Для поиска лучшего кадра для PnP берется метрика (число инлаеров) / (число 3d-2d соответствий),
         число инлаеров определяется с помощью solvePnPRansac
         """
         best_frame = None
+        points_3d_ids = np.sort(np.array(list(self._points_3d.keys())))
+
+        # max_cnt = self._other_parameters['points3dCntForBestFrame']
+        # points_3d_ids_subset = np.sort(
+        #     points_3d_ids[
+        #         np.random.choice(
+        #             points_3d_ids.shape[0],
+        #             min(points_3d_ids.shape[0], max_cnt),
+        #             replace=False
+        #         )
+        #     ]
+        # )
 
         for i, corners in enumerate(self._corner_storage):
             if self._handled_frames[i]:
                 continue
 
-            corresps = build_3d_2d_correspondences(
-                self._points_3d,
+            corresps = self._build_3d_2d_correspondences(
+                points_3d_ids,
                 corners,
             )
 
@@ -360,6 +387,11 @@ class CameraTracker:
         self._poses[frame_2] = view_mat_2
 
     def camera_tracking(self) -> Tuple[List[Pose], PointCloud]:
+        if self._known_view_1 is None or self._known_view_2 is None:
+            view_mats = self.init_view_mats()
+            self._known_view_1 = (view_mats.frame_1, view_mats.view_mat_1)
+            self._known_view_2 = (view_mats.frame_2, view_mats.view_mat_2)
+
         self._init_points_3d()
 
         while True:
@@ -398,13 +430,148 @@ class CameraTracker:
 
         return list(map(view_mat3x4_to_pose, self._poses)), point_cloud
 
+    def _find_essent_mat(
+            self,
+            corners_1: FrameCorners,
+            corners_2: FrameCorners,
+    ) -> Tuple[np.ndarray, Correspondences, float]:
+        correspondences = build_correspondences(corners_1, corners_2)
+        E, essent_mat_inliers = cv2.findEssentialMat(
+            correspondences.points_1,
+            correspondences.points_2,
+            method=cv2.RANSAC,
+            cameraMatrix=self._intrinsic_mat,
+            **self._init_view_mats_parameters['findEssentialMatrixParams'],
+        )
+
+        correspondences = remove_correspondences_with_ids(
+            correspondences,
+            np.argwhere(essent_mat_inliers.flatten() == 0),
+        )
+
+        _, hom_mat_inliers = cv2.findHomography(
+            correspondences.points_1,
+            correspondences.points_2,
+            method=cv2.RANSAC,
+            **self._init_view_mats_parameters['findHomographyParams'],
+        )
+
+        return E, correspondences, np.sum(hom_mat_inliers) / np.sum(essent_mat_inliers)
+
+    def _find_initial_view_mats(
+            self,
+            correspondences: Correspondences,
+            essential_mat: np.ndarray,
+    ) -> Tuple[np.ndarray, np.ndarray, int]:
+        R_1, R_2, t = cv2.decomposeEssentialMat(essential_mat)
+
+        view_mats = [
+            np.hstack((R_1, t)),
+            np.hstack((R_2, t)),
+            np.hstack((R_1, -t)),
+            np.hstack((R_2, -t))
+        ]
+
+        view_mat_1 = eye3x4()
+        best_view_mat = None
+        best_points_3d_cnt = None
+
+        for view_mat_2 in view_mats:
+            points_3d, _, _ = triangulate_correspondences(
+                correspondences,
+                view_mat_1,
+                view_mat_2,
+                self._intrinsic_mat,
+                self._triang_parameters,
+            )
+
+            if best_view_mat is None or points_3d.shape[0] > best_points_3d_cnt:
+                best_view_mat = view_mat_2
+                best_points_3d_cnt = points_3d.shape[0]
+
+        return view_mat_1, best_view_mat, best_points_3d_cnt
+
+    def init_view_mats(self) -> ViewMatsContext:
+        best_inliers_ratio = None
+        best_essent_mat_context = None
+
+        best_points_3d_cnt = None
+        best_view_mats_context = None
+
+        for frame_1, corners_1 in enumerate(self._corner_storage):
+            for frame_2, corners_2 in enumerate(self._corner_storage):
+                if frame_2 <= frame_1:
+                    continue
+
+                if frame_2 - frame_1 > self._init_view_mats_parameters['framesWindowSize']:
+                    break
+
+                logger.info(f'Handling pair ({frame_1}, {frame_2}) for initial camera positions')
+
+                E, correspondences, inliers_ratio = self._find_essent_mat(
+                    corners_1,
+                    corners_2,
+                )
+
+                if best_essent_mat_context is None or inliers_ratio < best_inliers_ratio:
+                    best_essent_mat_context = EssentialMatContext(
+                        frame_1,
+                        frame_2,
+                        E,
+                        correspondences
+                    )
+
+                    best_inliers_ratio = inliers_ratio
+
+                if inliers_ratio > self._init_view_mats_parameters['inliersRatioThres']:
+                    continue
+
+                view_mat_1, view_mat_2, points_3d_cnt = self._find_initial_view_mats(correspondences, E)
+
+                if best_view_mats_context is None or best_points_3d_cnt < points_3d_cnt:
+                    best_view_mats_context = ViewMatsContext(
+                        frame_1,
+                        frame_2,
+                        view_mat3x4_to_pose(view_mat_1),
+                        view_mat3x4_to_pose(view_mat_2)
+                    )
+                    best_points_3d_cnt = points_3d_cnt
+
+        if best_view_mats_context is None:
+            view_mat_1, view_mat_2, _ = self._find_initial_view_mats(
+                best_essent_mat_context.correspondences,
+                best_essent_mat_context.E,
+            )
+
+            return ViewMatsContext(
+                best_essent_mat_context.frame_1,
+                best_essent_mat_context.frame_2,
+                view_mat3x4_to_pose(view_mat_1),
+                view_mat3x4_to_pose(view_mat_2),
+            )
+
+        return best_view_mats_context
+
 
 CAMERA_TRACKER_PARAMETERS = {
-    'maxReprojError': 7.5,
+    'maxReprojError': 3.5,
     'PnPRansacParams': {
         'flags': cv2.SOLVEPNP_EPNP,
         'iterationsCount': 100,
-        'reprojectionError': 7.5,
+        'reprojectionError': 3.5,
+    },
+    'points3dCntForBestFrame': 200,
+}
+
+INIT_VIEW_MATS_PARAMETERS = {
+    'inliersRatioThres': 0.5,
+    'framesWindowSize': 50,
+    'findEssentialMatrixParams': {
+        'prob': 0.9999,
+        'threshold': 3.5,
+    },
+    'findHomographyParams': {
+        'ransacReprojThreshold': 3.5,
     },
 }
 
@@ -417,24 +584,22 @@ def track_and_calc_colors(camera_parameters: CameraParameters,
                           known_view_1: Optional[Tuple[int, Pose]] = None,
                           known_view_2: Optional[Tuple[int, Pose]] = None) \
         -> Tuple[List[Pose], PointCloud]:
-    if known_view_1 is None or known_view_2 is None:
-        raise NotImplementedError()
-
     rgb_sequence = frameseq.read_rgb_f32(frame_sequence_path)
     intrinsic_mat = to_opencv_camera_mat3x3(
         camera_parameters,
         rgb_sequence[0].shape[0]
     )
-
     camtracker = CameraTracker(
         rgb_sequence,
         corner_storage,
         intrinsic_mat,
-        known_view_1,
-        known_view_2,
         TRIANG_PARAMETERS,
         CAMERA_TRACKER_PARAMETERS,
+        known_view_1,
+        known_view_2,
+        INIT_VIEW_MATS_PARAMETERS,
     )
+
     return camtracker.camera_tracking()
 
 
